04dd358e1ba5130e17ffb76c5cff45a3702f2fc6
Aaron Beall <contact@abeall.com>
2025-04-08 21:31:12 -0400

    Frame out project structure

---
+++ src/index.ts
@@ -0,0 +1,10 @@
+export interface Point {
+  x: number;
+  y: number;
+}
+
+export const distance = (p1: Point, p2: Point): number => {
+  const dx = p2.x - p1.x;
+  const dy = p2.y - p1.y;
+  return Math.sqrt(dx * dx + dy * dy);
+};
